98972acfc85341f6e4633fb4e1563cf298704a08
jwhitlow45 <jwhitlow45@gmail.com>
2021-04-30 04:46:54 -0700

    bug:fixed name conflict  feat:used define for register count

--- include/RegFile.hpp
+++ include/RegFile.hpp
@@ -1,7 +1,16 @@
 #pragma once
+#define NUMREG 8
+
+#include<cstddef>
 
 class RegFile
 {
 public:
-    int regfile[8];
+    int reg[NUMREG];
+
+    RegFile()
+    {
+        for (size_t i = 0; i < NUMREG; i++)
+            reg[i] = 0;
+    }
 };
