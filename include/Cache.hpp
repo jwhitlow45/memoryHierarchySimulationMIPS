34194313fc9984e0abdca8500a20587520c100f1
jwhitlow45 <jwhitlow45@gmail.com>
2021-05-01 20:59:16 -0700

    feat:added functionality to set block as valid

--- include/Cache.hpp
+++ include/Cache.hpp
@@ -7,5 +7,6 @@ public:
     Block sets[8][2];
 
     void setBlockHistory(int, int); //set history bits based on lru
+    void setBlockValid(int, int);   //set block as valid
     int getVictimBlock(int);        //return mose recent block
 };
--- src/Cache.cpp
+++ src/Cache.cpp
@@ -6,6 +6,11 @@ void Cache::setBlockHistory(int set, int blockToSet)
     sets[set][!blockToSet].history = 0;
 }
 
+void Cache::setBlockValid(int set, int blockToSet)
+{
+    sets[set][blockToSet].valid = 1;
+}
+
 int Cache::getVictimBlock(int setNumber)
 {
     if (sets[setNumber][0].history == 1)
