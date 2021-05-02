506ee79a635c8cbd7ed92f40a3c86b7fe883375b
jwhitlow45 <jwhitlow45@gmail.com>
2021-05-01 20:29:40 -0700

    feat:added method to allow printing of memory contents

--- include/Memory.hpp
+++ include/Memory.hpp
@@ -13,4 +13,6 @@ public:
         for (size_t i = 0; i < MEMSIZE; i++) // init memory to mem location + 5
             memory[MEMSIZE] = MEMSIZE + 5;
     }
+
+    void printMemory();
 };
--- src/Memory.cpp
+++ src/Memory.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 #include "Memory.hpp"
+
+using namespace std;
+
+void Memory::printMemory()
+{
+    for (size_t i = 0; i < MEMSIZE; i++)
+    {
+        cout << memory[i] << '\t';
+        if (i % 16 == 0)
+            cout << endl;
+    }
+}
