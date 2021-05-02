7c45e2205fc6e7e4d7b736819c5e76d34a4d69a8
jwhitlow45 <jwhitlow45@gmail.com>
2021-05-01 20:26:37 -0700

    feat:added implementation to check which block is the victim block

--- include/Cache.hpp
+++ include/Cache.hpp
@@ -7,4 +7,5 @@ public:
     Block sets[8][2];
 
     void setBlockHistory(int, int); //set history bits based on lru
+    int getVictimBlock(int);        //return mose recent block
 };
--- src/Cache.cpp
+++ src/Cache.cpp
@@ -5,3 +5,10 @@ void Cache::setBlockHistory(int set, int blockToSet)
     sets[set][blockToSet].history = 1;
     sets[set][!blockToSet].history = 0;
 }
+
+int Cache::getVictimBlock(int setNumber)
+{
+    if (sets[setNumber][0].history == 1)
+        return 1;
+    return 0;
+}
